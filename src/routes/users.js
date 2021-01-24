50b50c93185a6657e35b4ca2878dba2789f65259
xuech <xuechunhua@med-log.cn>
2021-01-24 20:23:49 +0800

    router

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,8 +6,20 @@ router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+router.get('/bar/:name/:age', function (ctx, next) {
+  const { name,age } = ctx.params
+  ctx.body = {
+    name,
+    age
+  }
+})
+
+router.post('/login', async (ctx, next) => {
+  const { username, password } = ctx.request.body
+  ctx.body = {
+    username,
+    password
+  }
 })
 
 module.exports = router
