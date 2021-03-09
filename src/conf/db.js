ea8463fe1f82f376e4f83523aba97f2abda578e3
xuech <xuech_love@126.com>
2021-03-09 15:20:37 +0800

    线上环境配置

--- src/conf/db.js
+++ src/conf/db.js
@@ -1,4 +1,4 @@
-// const { isProd } = require('../utils/env')
+const { isProd } = require('../utils/env')
 /**
  * @description mysql配置
 */
@@ -12,6 +12,24 @@ let MYSQL_CONF = {
 let REDIS_CONF = {
     host: '127.0.0.1',
     prot: '6379'
+}
+
+if (isProd) {
+    REDIS_CONF = {
+        // 线上的 redis 配置
+        port: 6379,
+        host: '127.0.0.1'
+    }
+
+    MYSQL_CONF = {
+        // 线上的 mysql 配置
+        host: '114.215.182.183',
+        user: 'root',
+        password: '123456',
+        port: '3306',
+        database: 'koa2_weibo_db'
+    }
+    
 }
 
 module.exports = {
